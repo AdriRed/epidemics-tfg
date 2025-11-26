0fa56c0e6c3ef2535403b9097b17cfb835277125
AdriRed <adriamilmarcos@gmail.com>
2025-11-26 20:29:41 +0100

    added multiple test

--- main.f90
+++ main.f90
@@ -10,7 +10,17 @@ program main
 
    write(*, *) 'NEIGHBOUR LENGTHS', size(net%neighbours)
 
-   write(*, *) net%get_neighbours(6)
+   write(*, *) 1, "->", net%get_neighbours(1)
+   write(*, *) 2, "->", net%get_neighbours(2)
+   write(*, *) 3, "->", net%get_neighbours(3)
+   write(*, *) 4, "->", net%get_neighbours(4)
+   write(*, *) 5, "->", net%get_neighbours(5)
+   write(*, *) 6, "->", net%get_neighbours(6)
+   write(*, *) 7, "->", net%get_neighbours(7)
+   write(*, *) 8, "->", net%get_neighbours(8)
+   write(*, *) 9, "->", net%get_neighbours(9)
+   write(*, *) 10, "->", net%get_neighbours(10)
+   write(*, *) 11, "->", net%get_neighbours(11)
 
    close(unit=11)
 end program main

0d52984a18a1b5b0760335178a38d68e344ccd53
AdriRed <adriamilmarcos@gmail.com>
2025-11-28 13:30:54 +0100

    added function for checking memory used

--- include/net_loader.f90
+++ include/net_loader.f90
@@ -1,13 +1,14 @@
 module net_loader
    use fhash
+   use iso_fortran_env, only: int64
    implicit none
 
    type epidemic_net
       integer, allocatable :: neighbours(:), starter_ptrs(:), end_ptrs(:), degree(:)
       integer :: nodes_count, links_count
       type(int_int_hashmap), private :: hashmap
-      contains
-         procedure, public :: get_neighbours
+   contains
+      procedure, public :: get_neighbours
 
 
    end type epidemic_net
@@ -26,15 +27,50 @@ contains
    type(epidemic_net) function initialize_net(unit) result(net)
       integer, intent(in) :: unit
       call init_hashmap(unit, net)
-      
+      write(*, *) 'Initialized hash map'
+      write(*, *) '---- nodes count -> ', net%nodes_count
+      write(*, *) '---- links count -> ', net%links_count
+
       allocate(net%neighbours(2*net%links_count), &
          net%starter_ptrs(net%nodes_count), &
          net%end_ptrs(net%nodes_count), &
          net%degree(net%nodes_count))
 
       call init_degrees_pointers(unit, net)
+      write(*, *) 'Initialized degrees and pointers'
+
       call init_neighbours(unit, net)
+      write(*, *) 'Initialized neighbour array'
+
    end function initialize_net
+
+   integer(int64) function net_memory_bytes(net) result(total)
+      type(epidemic_net), intent(in) :: net
+      integer(int64) :: bytes_element
+
+      total = 0
+
+      ! neighbours
+      bytes_element = storage_size(net%neighbours) / 8
+      total = total + size(net%neighbours, kind=int64) * bytes_element
+
+      ! starter_ptrs
+      bytes_element = storage_size(net%starter_ptrs) / 8
+      total = total + size(net%starter_ptrs, kind=int64) * bytes_element
+
+      ! end_ptrs
+      bytes_element = storage_size(net%end_ptrs) / 8
+      total = total + size(net%end_ptrs, kind=int64) * bytes_element
+
+      ! degree
+      bytes_element = storage_size(net%degree) / 8
+      total = total + size(net%degree, kind=int64) * bytes_element
+
+      ! Hashmap → depende de fhash (tienes que mirar su implementación)
+      ! Si tiene allocatables internos, habrá que sumarlos igual que arriba
+
+   end function net_memory_bytes
+
 
    subroutine init_degrees_pointers(unit, net)
       integer, intent(in) :: unit
@@ -64,22 +100,22 @@ contains
    end subroutine init_degrees_pointers
 
    integer function count_lines(unit) result(retval)
-        integer, intent(in) :: unit
-        integer :: iostat
-        retval = 0
-        rewind(unit)
+      integer, intent(in) :: unit
+      integer :: iostat
+      retval = 0
+      rewind(unit)
 
-        loop: do 
-            read(unit, *, iostat=iostat) 
-            if (iostat < 0) then
-                exit loop 
-            else
-                retval = retval + 1
-            end if
-        end do loop
+      loop: do
+         read(unit, *, iostat=iostat)
+         if (iostat < 0) then
+            exit loop
+         else
+            retval = retval + 1
+         end if
+      end do loop
 
-        return
-    end function count_lines
+      return
+   end function count_lines
 
    subroutine init_hashmap(unit, net)
       integer, intent(in) :: unit
@@ -89,7 +125,7 @@ contains
       i = 1
       lines = count_lines(unit)
       rewind(unit)
-      call net%hashmap%reserve(lines) ! reserve 1E8 entries in nodes
+      call net%hashmap%reserve(lines) ! reserve N approx E nodes
       loop: do
          read(unit, *, iostat=iostat) node_a, node_b
          if (iostat < 0) then
@@ -125,7 +161,7 @@ contains
          else
             call net%hashmap%get(node_a, index_node_a)
             call net%hashmap%get(node_b, index_node_b)
-            
+
             net%end_ptrs(index_node_a) = net%end_ptrs(index_node_a) + 1
             net%end_ptrs(index_node_b) = net%end_ptrs(index_node_b) + 1
             net%neighbours(net%end_ptrs(index_node_a)) = index_node_b
--- main.f90
+++ main.f90
@@ -4,23 +4,10 @@ program main
 
    type(epidemic_net) net
 
-   open(unit=11, file='../files/test-file-1.txt', action='read')
+   open(unit=11, file='../ignore-files/large_twitch_edges.csv', action='read')
 
    net = initialize_net(11)
-
-   write(*, *) 'NEIGHBOUR LENGTHS', size(net%neighbours)
-
-   write(*, *) 1, "->", net%get_neighbours(1)
-   write(*, *) 2, "->", net%get_neighbours(2)
-   write(*, *) 3, "->", net%get_neighbours(3)
-   write(*, *) 4, "->", net%get_neighbours(4)
-   write(*, *) 5, "->", net%get_neighbours(5)
-   write(*, *) 6, "->", net%get_neighbours(6)
-   write(*, *) 7, "->", net%get_neighbours(7)
-   write(*, *) 8, "->", net%get_neighbours(8)
-   write(*, *) 9, "->", net%get_neighbours(9)
-   write(*, *) 10, "->", net%get_neighbours(10)
-   write(*, *) 11, "->", net%get_neighbours(11)
+   write(*, *) 'Memory used (bytes): ', net_memory_bytes(net)
 
    close(unit=11)
 end program main

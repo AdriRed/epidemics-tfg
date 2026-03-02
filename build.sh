22eedf25885dc6fa61e92c95270c2d35603b1bb6
AdriRed <adriamilmarcos@gmail.com>
2026-03-02 12:19:36 +0100

    updated settings and added support for compiling reversed_skiplist

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ mkdir -p ./builds
 
 gfortran -O3 -march=native -funroll-loops \
     ./include/mt19937.f90 ./include/mt19937_par.f90 ./include/fhash.f90 \
-    ./include/net_loader.f90 ./include/epidemic.f90 \
+    ./include/net_loader.f90 ./include/reversed_skiplist.f90 ./include/epidemic.f90 \
     ./main.f90 \
     -o ./builds/main.out
